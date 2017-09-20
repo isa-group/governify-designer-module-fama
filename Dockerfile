abf1893e469b45cab6f4a66c84edd78c0868b8f4
Felipe Vieira da Cunha Serafim <feserafim@gmail.com>
2017-09-20 16:48:42 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM jagalindo/fama-shell:firsttry
+
+# Install nodejs
+RUN apt-get install -y curl && curl -sL https://deb.nodesource.com/setup_6.x | bash
+RUN apt-get install -y nodejs && apt-get clean
+
+# Start MiniZinc service
+RUN mkdir governify-designer-module-minizinc
+WORKDIR /tmp/governify-designer-module-minizinc
+ADD . ./
+RUN npm install
+EXPOSE 80 443
+CMD node index.js
---
+++ operations.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const yaml = require('js-yaml');
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
+const FaMaTools = require("E:\\Documents\\Coding\\FaMa\\governify-fama-tools\\index.js");
+const Reasoner = FaMaTools.Reasoner;
+
+module.exports = {
+
+    products: function (res, data, parameters) {
+        res.send(new responseModel('OK', "hello world", data, null));
+    },
+    executeDocument: function (res, data, parameters) {
+        var famaDocument = data[0].content;
+
+        var reasoner = new Reasoner({
+            type: 'local',
+            folder: 'fama_files'
+        });
+
+        reasoner.solve(famaDocument, (err, stdout, stderr, isSatisfiable, document) => {
+            if (err || (parameters && parameters.format === "json")) {
+                res.send({
+                    error: err,
+                    stdout: stdout,
+                    stderr: stderr,
+                    isSatisfiable: isSatisfiable,
+                    document: document
+                });
+            } else {
+                res.send(new responseModel('OK', getResponseModelCSP(stdout), data, null));
+            }
+        });
+
+    },
+    check: function (syntax, res, data) {
+        res.status(200).json(new responseModel('OK', null, null, null));
+    },
+    checkConsistency: function (syntax, res, data) {
+        res.status(200).json(new responseModel('OK', null, null, null));
+    },
+    translate: function (syntaxSrc, syntaxDes, res, data) {
+        res.status(200).json(new responseModel('OK', "Nothing to translate", null, []));
+    }
+}
+
+function translateCombinationError(res, syntaxDes) {
+    res.status(200).json(new responseModel("ERROR", "It is not possible to translate from yaml to " + syntaxDes, null, []));
+}
+
+function responseModel(status, message, data, annotations) {
+    this.status = status;
+    this.message = message;
+    this.data = data;
+    this.annotations = annotations;
+}
+
+function annotation(type, row, column, text) {
+    this.type = type;
+    this.row = row;
+    this.column = column;
+    this.text = text;
+}
+
+let getResponseModelCSP = (stdout) => {
+    return '<pre>' + stdout + '</pre>';
+};
